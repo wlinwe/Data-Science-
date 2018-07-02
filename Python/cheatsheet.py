0e75eeb7b4f25f038f8f29e4924468b97709ebc5
Wendy Lin <40526534+wlinwe@users.noreply.github.com>
2018-07-01 23:02:14 -0500

    Update cheatsheet.py

--- Python/cheatsheet.py
+++ Python/cheatsheet.py
@@ -9,3 +9,14 @@ def dictionary:
   for p in range(len(order_sum)):
     d[order_sum.index[p]]=order_sum[p]
 d
+###counting 
+burrito=0
+topping=0
+for i in range(data.shape[0]):
+  if 'Burrito' in data.iloc[i,2]:
+    burrito+=1
+    topping+=data.iloc[i,3].count(',')+1
+
+quantities=topping/burrito
+quantities  
+#except counting what actual topping is, count the delimiter will be easier.
