52821bd953450ccee52b4b26d19098cd9b14e023
wlinwe <40526534+wlinwe@users.noreply.github.com>
2018-07-01 22:35:25 -0500

    Create cheatsheet.py

---
+++ cheatsheet.py
@@ -0,0 +1,11 @@
+##**Some tips for data analysis(common use syntax):**
+###1. create dictionary:
+#list can be a array or simple value
+d={'key':list}
+d['key']=value
+#use two matched array create dictionary:
+def dictionary:
+  d={}
+  for p in range(len(order_sum)):
+    d[order_sum.index[p]]=order_sum[p]
+d
